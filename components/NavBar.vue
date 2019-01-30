ff70170c186f7fd40ad12bf0afd89a65b8eba3cf
Gregg Pollack <greggpollack+github@gmail.com>
2019-01-30 16:58:42 -0500

    Simplified pages, added layout, added NavBar

---
+++ components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="nav">
+    <nuxt-link to="/" class="brand">
+      Real World Events
+    </nuxt-link>
+    <nav>
+      <nuxt-link to="/">
+        List
+      </nuxt-link> | 
+      <nuxt-link to="/create">
+        Create
+      </nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.brand {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: 1.5em;
+  color: #39b982;
+  text-decoration: none;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+}
+.nav .nav-item {
+  box-sizing: border-box;
+  margin: 0 5px;
+  color: rgba(0, 0, 0, 0.5);
+  text-decoration: none;
+}
+.nav .nav-item.router-link-exact-active {
+  color: #39b982;
+  border-bottom: solid 2px #39b982;
+}
+
+.nav a {
+  display: inline-block;
+}
+</style>
--- pages/create.vue
+++ pages/create.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Events</h1>
+    <h1>Create an Event</h1>
   </div>
 </template>
