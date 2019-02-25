96f2288decfb9ba8b6dfd570a60eea690f889cd3
Gregg Pollack <greggpollack+github@gmail.com>
2019-02-25 13:20:58 -0500

    Added SEO to dynamic route

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <h1>Event #{{ this.$route.params.id }}</h1>
+    <h1>Event #{{ id }}</h1>
   </div>
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'What you need to know about event #' + this.id
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
