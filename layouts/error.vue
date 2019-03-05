2cce0c4bbb0f381511624e90bb5c925b125e1aa9
Gregg Pollack <greggpollack+github@gmail.com>
2019-03-05 00:11:28 -0500

    Added custom error

---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="#DBE1EC" viewBox="0 0 48 48">
+      <path d="M22 30h4v4h-4zm0-16h4v12h-4zm1.99-10C12.94 4 4 12.95 4 24s8.94 20 19.99 20S44 35.05 44 24 35.04 4 23.99 4zM24 40c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z" />
+    </svg>
+
+    <div class="title">
+      {{ message }}
+    </div>
+    <p v-if="statusCode === 404">
+      <nuxt-link to="/">
+        Return to homepage
+      </nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  head() {
+    return {
+      title: this.message
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message() {
+      return this.error.message
+    }
+  }
+}
+</script>
