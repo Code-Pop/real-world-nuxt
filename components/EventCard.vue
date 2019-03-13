66e25b2eea726e7166f8e65eedb01285c1c8c23f
Gregg Pollack <greggpollack+github@gmail.com>
2019-03-13 15:03:50 -0400

    Now with EventCard and Axios API call

---
+++ components/EventCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <nuxt-link :to="'/event/' + event.id">
+    <div class="-shadow">
+      <span class="eyebrow">
+        @{{ event.time }} on {{ parsedDate }}
+      </span>
+      <h4 class="title">
+        {{ event.title }}
+      </h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </nuxt-link>
+</template>
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    event: Object
+  },
+  computed: {
+    parsedDate() {
+      const eventDate = new Date(this.event.date)
+      return eventDate.toDateString()
+    }
+  }
+}
+</script>
+<style scoped>
+div {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+div:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.title {
+  margin: 0;
+  color: black;
+}
+a {
+  text-decoration: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,40 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
   head() {
     return {
       title: 'Event Listing'
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.'
+        })
+      })
+  },
+  components: {
+    EventCard
   }
 }
 </script>
