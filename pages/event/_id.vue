046a7233ae0235e31ada1dd1a1b995883d21a4cd
Gregg Pollack <greggpollack+github@gmail.com>
2019-03-17 22:41:08 -0400

    API call for event show page

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,25 +1,35 @@
 <template>
   <div>
-    <h1>Event #{{ id }}</h1>
+    <h1>{{ event.title }}</h1>
   </div>
 </template>
 <script>
 export default {
   head() {
     return {
-      title: 'Event #' + this.id,
+      title: this.event.title,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: 'What you need to know about event #' + this.id
+          content: 'What you need to know about ' + this.event.title
         }
       ]
     }
   },
-  computed: {
-    id() {
-      return this.$route.params.id
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await $axios.get(
+        'http://localhost:3000/events/' + params.id
+      )
+      return {
+        event: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch event #' + params.id
+      })
     }
   }
 }
