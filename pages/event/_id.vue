bcb19705c8f3f3cdd448da0e4eef8af02f8512a7
Gregg Pollack <greggpollack+github@gmail.com>
2019-05-15 12:15:06 -0400

    Configuration to deploy to heroku

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+  <div>
     <div class="event-header">
       <span class="eyebrow">
         @{{ event.time }} on {{ event.date }}
--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `http://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
