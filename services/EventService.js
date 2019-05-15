73476c5c248f5c87b5e7317f88238fa2de8b6c7a
Gregg Pollack <greggpollack+github@gmail.com>
2019-05-15 12:18:06 -0400

    Now with https

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
+  baseURL: `https://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
